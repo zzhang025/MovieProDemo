33220639d56e32056ad5f344701a76b7aea6365d
zzhang025 <zhangzx45@gmail.com>
2022-11-03 10:36:51 -0700

    11.3

---
+++ MovieProDemo/Controllers/ActorsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieProDemo.Services.Interfaces;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieProDemo.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+            return View(actor);
+        }
+    }
+}
+
