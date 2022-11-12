e2a5b1e018a4119a52b32fcd24f2cea6477e2775
zzhang025 <zhangzx45@gmail.com>
2022-11-12 11:16:28 -0800

    wrap_up

--- MovieProDemo/Controllers/ActorsController.cs
+++ MovieProDemo/Controllers/ActorsController.cs
@@ -20,7 +20,7 @@ namespace MovieProDemo.Controllers
             _mappingService = mappingService;
         }
 
-        public async Task<IActionResult> Detail(int id)
+        public async Task<IActionResult> Details(int id)
         {
             var actor = await _tmdbMovieService.ActorDetailAsync(id);
             actor = _mappingService.MapActorDetail(actor);
